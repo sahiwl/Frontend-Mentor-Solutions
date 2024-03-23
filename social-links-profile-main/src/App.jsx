7f9962901841f5a720b8f184c33d746baf05b460
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-03-23 15:21:54 +0530

    completed social-links-profile challenge

---
+++ social-links-profile-main/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <div className="card">
+        <div className="card-container">
+          <div className="card1">
+          <img src="./avatar-jessica.jpeg"></img>
+          <span className="name">Jessica Randall</span>
+          <span className="location">London, United Kingdom</span>
+          <span className="occ">"Front-end developer and avid reader."</span>
+        </div>
+        <div className="card2">
+          <span className="box">Github</span>
+          <span className="box">Frontend Mentor</span>
+          <span className="box">LinkedIn</span>
+          <span className="box">Twitter</span>
+          <span className="box">Instagram</span>
+        </div>
+      </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
